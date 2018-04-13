2b1200f492c4c29667650594ddb9592769fffd2e
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-04-13 15:06:50 -0700

    first

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-var numArray = [52,79,33,1,108, 689789797,32435446,5675757,23424,4567890,1234567890,098765432];
-var ascendingOrder = function( a, b){
-    return a - b ;
+// var numArray = [52,79,33,1,108, 689789797,32435446,5675757,23424,4567890,1234567890,098765432];
+// var ascendingOrder = function( a, b){
+//     return a - b ;
+// };
+// var descendingOrder = function( a, b){
+//     return b - a ;
+// };
+// console.log (numArray.sort(ascendingOrder));
+// console.log (numArray.sort(descendingOrder));
+
+var numArray = [30,99,48,334,55,566];
+
+var copiedArray = function (someArray){
+
+  var newArray =[];
+
+    for (var i= 0 ; i < someArray.length; i++){
+        newArray.push(someArray[i]);
+}
+return newArray.reverse();
 };
-var descendingOrder = function( a, b){
-    return b - a ;
-};
-console.log (numArray.sort(ascendingOrder));
-console.log (numArray.sort(descendingOrder));
+
+var reversedArray = copiedArray(numArray);
